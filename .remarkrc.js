0ca58cd03ecc7fe01733a46b2af1bc6b8070add4
Philipp Baschke <philipp@baschke.com>
2016-05-25 09:46:26 +0200

    Setup Markdown linting with remark-lint
    I chose the .js form of .remarkrc to get syntax highlighting when
    editing the file.
    
    The --quiet flag only outputs warnings and errors and --frail makes sure
    that remark returns 1 when warnings occur (instead of return 1 only on
    errors) which leads to failed builds on warnings.
    
    The package npm-run-all makes running npm tasks in parallel possible. I
    used run-p as the shorter version of npm-run-all --parallel.

---
+++ .remarkrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "plugins": {
+    "lint": {
+      "blockquote-indentation": 2,
+      "checkbox-character-style": {
+        "checked": "x",
+        "unchecked": " "
+      },
+      "code-block-style": "fenced",
+      "emphasis-marker": "*",
+      "fenced-code-flag": [
+          "css",
+          "js",
+          "less",
+          "scss",
+          "shell"
+      ],
+      "fenced-code-marker": "`",
+      "heading-style": "atx",
+      "link-title-style": "'",
+      "list-item-indent": "space",
+      "ordered-list-marker-style": ".",
+      "rule-style": "---",
+      "strong-marker": "*",
+      "table-cell-padding": "padded",
+      "unordered-list-marker-style": "-"
+    }
+  }
+}
